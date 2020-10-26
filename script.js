9c6d079cd8c337e3924a4ea07dc4cd570379d994
MaxYashch <maxyashch@gmail.com>
2020-10-26 19:29:19 +0300

    added API, reading and deploing json

--- script.js
+++ script.js
@@ -1,37 +1,56 @@
-window.addEventListener('load', () => {
-    let long;
+$(document).ready(function () {
     let lat;
-    let temperatureDescription = document.querySelector('.temperature__description');
-    let temperatureDegree = document.querySelector('.temperature__degree');
-    let locationTimezone = document.querySelector('.location__timezone');
-
+    let long;
 
     if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(position => {
-            console.log(position);
-            long = position.coords.longitude;
+
+        navigator.geolocation.getCurrentPosition(function (position) {
+
             lat = position.coords.latitude;
+            long = position.coords.longitude;
 
-            var api = 'https://fcc-weather-api.glitch.me/api/current?lat=' + lat + '&lon=' + long + '';
-            // console.log(typeof api);
+            const api = 'https://fcc-weather-api.glitch.me/api/current?lat=' + lat + '&lon=' + long + '';
 
-            fetch(api)
-                .then(response => {
-                    // console.log(response);
-                    return response.json();
-                })
-                .then(data => {
-                    console.log(data);
-                    const { temperature, summary } = data.currently;
-                    // set DOM elements from API
-                    temperatureDegree.textContent = temperature;
-                    temperatureDescription.textContent = summary;
-                    locationTimezone.textContent = data.timezone;
-                })
+            $.getJSON(api, function (responce) {
+
+                let celsius = responce.main.temp;
+                let farenheit = (celsius * 1.8) + 32;
+
+                let location = responce.name;
+
+                $('.weather__location-point').html(location);
+                $('.weather__temperature-point').html(Math.floor(celsius));
+                $('.weather__description').html(responce.weather[ 0 ].description);
+                $('.weather__location-type').attr('id', responce.weather[ 0 ].main);
+                $('.weather__temperature').on('click', function () {
+                    if ($('.teweather__temperature-pointmp').html() == (Math.floor(celsius))) {
+                        $('.weather__temperature-point').html(Math.floor(farenheit));
+                        $('.weather__temperature-type').html('°F');
+
+                    } else {
+                        $('.weather__temperature-point').html(Math.floor(celsius));
+                        $('.weather__temperature-type').html('°C');
+                    }
+                });
+
+                //SETTING UP THE ICON 
+                let icons = new Skycons({
+                    "color": "white"
+                });
+
+                icons.set("Clear", Skycons.CLEAR_DAY);
+                icons.set("Clear-night", Skycons.CLEAR_NIGHT);
+                icons.set("Partly-cloudy-day", Skycons.PARTLY_CLOUDY_DAY);
+                icons.set("Partly-cloudy-night", Skycons.PARTLY_CLOUDY_NIGHT);
+                icons.set("Clouds", Skycons.CLOUDY);
+                icons.set("Rain", Skycons.RAIN);
+                icons.set("Sleet", Skycons.SLEET);
+                icons.set("Snow", Skycons.SNOW);
+                icons.set("Wind", Skycons.WIND);
+                icons.set("Fog", Skycons.FOG);
+                icons.play();
+
+            });
         });
-
-
-    } else {
-        h1.textContent = 'Does not work because ....'
     }
 });
